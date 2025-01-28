2819399387fe17db4746f41763ebb6a544a71d91
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-01-28 18:52:24 +0100

    Successfully import games from json.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Programmation mobile
+Ce repository contient l'ensemble du code pour le projet réalisé dans le cadre du cours de programmation mobile à l'INSA de Rennes.
+
+## Eléments non réalisés
+
+## Eléments supplémentaires réalisés
+### fonctionnalités
+### Outils techniques
+
+## Améliorations possibles
--- app/src/main/java/com/insa/mygamelist/data/IGDB.kt
+++ app/src/main/java/com/insa/mygamelist/data/IGDB.kt
@@ -1,22 +1,107 @@
 package com.insa.mygamelist.data
 
 import android.content.Context
+import android.util.Log
 import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.google.gson.reflect.TypeToken
 import com.insa.mygamelist.R
 
 object IGDB {
 
     lateinit var covers: List<Cover>
+    lateinit var platformLogos: List<PlatformLogo>
+    lateinit var genres: List<Genre>
+    lateinit var platforms: List<Platform>
+    lateinit var games: List<Game>
 
     fun load(context: Context) {
+        // Import covers from Json
         val coversFromJson: List<Cover> = Gson().fromJson(
             context.resources.openRawResource(R.raw.covers).bufferedReader(),
             object : TypeToken<List<Cover>>() {}.type
         )
 
         covers = coversFromJson
+
+        // import Genres from Json
+        val genresFromJson: List<Genre> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.genres).bufferedReader(),
+            object : TypeToken<List<Genre>>() {}.type
+        )
+
+        genres = genresFromJson
+
+        // Import PlatformLogos from Json
+        val platformLogosFromJson: List<PlatformLogo> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platform_logos).bufferedReader(),
+            object : TypeToken<List<PlatformLogo>>() {}.type
+        )
+
+        platformLogos = platformLogosFromJson
+
+        // Import Platforms from Json
+        val rawPlatformsFromJson: List<RawPlatform> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.platforms).bufferedReader(),
+            object : TypeToken<List<RawPlatform>>() {}.type
+        )
+
+
+        // Fix references to platform logos into platforms
+        platforms = rawPlatformsFromJson.map { raw ->
+            val logo = platformLogos.find { raw.logoId == it.id }
+            if (logo == null) Log.e("IGDB", "No logo found for platform ${raw.name}")
+
+            Platform(raw.id, raw.name, logo)
+        }
+
+        // Import Games from Json
+        val gamesFromJson: List<RawGame> = Gson().fromJson(
+            context.resources.openRawResource(R.raw.games).bufferedReader(),
+            object : TypeToken<List<RawGame>>() {}.type
+        )
+
+        games = gamesFromJson.map { raw ->
+            val cover = covers.find { it.id == raw.cover }
+            val genres = genres.filter { raw.genres.contains(it.id) }
+            val platforms = platforms.filter { raw.platforms.contains(it.id) }
+            Game(raw.id, cover!!, raw.firstReleaseDate, genres, raw.name, platforms, raw.summary, raw.totalRating)
+        }
+
+        for (game in games) {
+            Log.d("IGDB", game.toString())
+        }
+
+        Log.d("IGDB", "Successfully loaded ${games.size} games")
+
     }
 }
 
 data class Cover(val id: Long, val url: String)
+data class Genre(val id: Long, val name: String)
+data class PlatformLogo(val id: Long, val url: String)
+data class RawPlatform(val id: Long, val name: String, @SerializedName("platform_logo") val logoId: Long)
+data class Platform(val id: Long, val name: String, val logo: PlatformLogo? = null)
+
+data class RawGame(
+    val id: Long,
+    val cover: Long? = null,
+    val firstReleaseDate: Int,
+    val genres: List<Long>,
+    val name: String,
+    val platforms: List<Long>,
+    val summary: String,
+    @SerializedName("total_rating")
+    val totalRating: Double
+)
+
+data class Game(
+    val id: Long,
+    val cover: Cover,
+    val firstReleaseDate: Int,
+    val genres: List<Genre>,
+    val name: String,
+    val platforms: List<Platform>,
+    val summary: String,
+    val totalRating: Double
+)
