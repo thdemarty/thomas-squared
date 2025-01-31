ed817283a69ea142e18859f9e4fee02a5dbe2511
ElectricCooper <146211024+ElectricCooper@users.noreply.github.com>
2025-01-31 13:34:04 +0100

    Clean Import in MainActivity

--- app/src/main/java/com/insa/mygamelist/MainActivity.kt
+++ app/src/main/java/com/insa/mygamelist/MainActivity.kt
@@ -4,22 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import com.insa.mygamelist.data.IGDB
 import com.insa.mygamelist.ui.components.AppNavHost
-import com.insa.mygamelist.ui.components.VideoGameCardList
 import com.insa.mygamelist.ui.theme.MyGamesListTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
 
 
