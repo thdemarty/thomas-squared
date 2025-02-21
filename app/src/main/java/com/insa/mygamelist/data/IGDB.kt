d082839a9de69a94e01c0be4504b1a9b721b56b1
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-02-21 23:32:27 +0100

    Add clientId and clientSecret hardcoded in the source code.... (sorry)

---
+++ app/src/main/java/com/insa/mygamelist/data/IGDB.kt
@@ -0,0 +1,9 @@
+package com.insa.mygamelist.data
+
+object IGDB {
+    /* Ugly af to have it hardcoded but anyway */
+    private const val clientId = "ufaqx982uctnp7m3l8mlj8adu3e9py"
+    private const val clientSecret = "rwbl2b9n4bc19f17fxivuj4vakzdrn"
+
+    // I will continue tomorrow i guess...
+}
